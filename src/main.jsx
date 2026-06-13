6717265d7565c9c5b00f797a584a8016bc48f247
Hrushikeshj26 <hrushij92@gmail.com>
2026-06-13 22:31:16 +0530

    Rounting Improved

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CartProvider } from './context/cartContext.jsx'
+import { AuthProvider } from './context/authContext.jsx'
 
-createRoot(document.getElementById('root')).render(<App/>)
+createRoot(document.getElementById('root')).render(
+    <AuthProvider>
+        <CartProvider>
+            <App/>
+        </CartProvider>
+    </AuthProvider>
+)
